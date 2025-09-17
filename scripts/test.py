a2c72c5be0fe1231e0142d6139facdcb00d7fc04
jonathan <jonathan.santos@picpaybank.com>
2025-09-17 11:53:26 -0300

    add a bunch of tests for error handling

--- scripts/test.py
+++ scripts/test.py
@@ -1,44 +1,48 @@
 #!/usr/bin/env python3
 
-from sys import argv, exit, stderr
-from os import listdir, path
+import base64
 import subprocess
 import time
+from os import listdir, path
+from sys import argv, exit, stderr
 
-def run_test(program_path: str, test_path: str) -> tuple[bool, str, str]:
+
+def decode64(s: str) -> str:
+    return base64.b64decode(bytes(s, "ascii")).decode("utf-8")
+
+def run_test(program_path: str, test_path: str) -> tuple[int, str, str]:
     result = subprocess.run(
         [program_path, "--file", test_path],
         capture_output=True,
         text=True
     )
 
-    return (result.returncode == 0, result.stdout, result.stderr)
+    return (result.returncode, result.stdout, result.stderr)
 
 
 def snap(tests_folder: str, program: str) -> None:
     files = listdir(tests_folder)
-    outs = []
+    outs = {}
     for file in files:
         if not path.isdir(file) and not file.endswith(".snap"):
             test_path = path.join(tests_folder, file)
-            ok, out, err = run_test(program, test_path)
-            if not ok:
-                print(
-                    f"ERROR: test {test_path} failed: {err}. Aborting snaping.",
-                    file=stderr
-                )
+            outs[test_path] = (run_test(program, test_path))
 
-            outs.append((test_path, out))
-
-    for test_resul in outs:
-        snap_path = test_resul[0] + ".snap"
+    for test_path in outs:
+        test_result = outs[test_path]
+        snap_path = test_path + ".snap"
         try:
             with open(snap_path, "w+") as f:
-                f.write(test_resul[1])
-
-            print(f"SNAPED OK: {test_resul[0]} -> {snap_path}")
+                f.writelines([
+                    str(test_result[0]),
+                    "\n",
+                    base64.b64encode(bytes(test_result[1], "utf-8")).decode("ascii"),
+                    "\n",
+                    base64.b64encode(bytes(test_result[2], "utf-8")).decode("ascii")
+                ])
+            print(f"SNAPED OK: {test_path} -> {snap_path}")
         except Exception as e:
-            print(f"SNAPED FAILED: {test_resul[0]} {e}")
+            print(f"SNAPED FAILED: {test_path} {e}")
 
 def run(tests_folder: str, program: str) -> None:
     files = listdir(tests_folder)
@@ -49,22 +53,36 @@ def run(tests_folder: str, program: str) -> None:
         if not path.isdir(file) and not file.endswith(".snap"):
             total += 1
             test_path = path.join(tests_folder, file)
-            ok, actual, err = run_test(program, test_path)
-            if not ok:
-                print(f"TEST FAILED -> {test_path}: {err}")
-            else:
-                try:
-                    with open(test_path + ".snap", "r") as f:
-                        expected = f.read()
-                        if expected != actual:
-                            print(f"TEST FAILED -> {test_path}")
-                            print(f"    Expected: {expected}")
-                            print(f"    Actual: {actual}")
-                        else:
-                            print(f"TEST PASSED -> {test_path}")
-                            passed += 1
-                except Exception as e:
-                    print(f"TEST FAILED -> {test_path}: {e}")
+            errcode, actual, err = run_test(program, test_path)
+            try:
+                with open(test_path + ".snap", "r") as f:
+                    expected = f.readlines()
+                    expected = list(map(lambda x: x.strip(), expected))
+                    expected_code = int(expected[0])
+                    expected_stdout = decode64(expected[1])
+                    expected_stderr = ''
+                    if len(expected) == 3:
+                        expected_stderr = decode64(expected[2])
+
+                    test_passed = (expected_code == errcode and
+                                   expected_stdout == actual and
+                                   expected_stderr == err)
+
+                    if test_passed:
+                        print(f"TEST PASSED -> {test_path}")
+                        passed += 1
+                    else:
+                        print(f"TEST FAILED -> {test_path}")
+                        print(f"    EXPECTED:")
+                        print(f"        ERRCODE: {expected_code}")
+                        print(f"        STDOUT : {expected_stdout}")
+                        print(f"        STDERR : {expected_stderr}")
+                        print(f"    ACTUAL:")
+                        print(f"        ERRCODE: {errcode}")
+                        print(f"        STDOUT : {actual}")
+                        print(f"        STDERR : {err}")
+            except Exception as e:
+                print(f"TEST FAILED -> {test_path}: {e}")
     end = time.perf_counter()
 
     print(
